dd4dea993eba5d05f558de7d0276b2cb19f4e61f
Jamy Chahal <41945728+KiloNovemberDelta@users.noreply.github.com>
2020-08-30 14:43:37 +0200

    Add principales python code

---
+++ aruco_detector.py
@@ -0,0 +1,30 @@
+import cv2
+import cv2.aruco as aruco
+
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    parameters =  aruco.DetectorParameters_create()
+
+    #lists of ids and the corners beloning to each id
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+    print(corners)
+
+    gray = aruco.drawDetectedMarkers(gray, corners)
+
+    #print(rejectedImgPoints)
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
